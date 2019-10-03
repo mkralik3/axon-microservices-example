55c0c600931ef7ad2727450288f7c062dd764639
mkralik3 <mkralik@redhat.com>
2019-10-03 20:19:25 +0200

    Update lra to the latest version

--- hotel-service-axon/src/main/java/org/mkralik/learning/axon/microservices/hotel/JerseyConfig.java
+++ hotel-service-axon/src/main/java/org/mkralik/learning/axon/microservices/hotel/JerseyConfig.java
@@ -1,5 +1,7 @@
 package org.mkralik.learning.axon.microservices.hotel;
 
+import io.narayana.lra.client.internal.proxy.nonjaxrs.LRAParticipantRegistry;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.wadl.internal.WadlResource;
 import org.springframework.stereotype.Component;
@@ -14,5 +16,12 @@ public class JerseyConfig extends ResourceConfig {
         register(io.narayana.lra.filter.ClientLRAResponseFilter.class);
         register(io.narayana.lra.filter.FilterRegistration.class);
         register(io.narayana.lra.filter.ServerLRAFilter.class);
+        register(new AbstractBinder(){
+            @Override
+            protected void configure() {
+                bind(LRAParticipantRegistry.class)
+                        .to(LRAParticipantRegistry.class);
+            }
+        });
     }
 }
