72eacb0d80cd9e5646512ce42230dedee4643487
mkralik3 <mkralik@redhat.com>
2019-10-13 16:33:17 +0200

    Update example according to connector changes + clear dependencies

--- hotel-service-axon/src/main/java/org/mkralik/learning/axon/microservices/hotel/HotelAxonApplication.java
+++ hotel-service-axon/src/main/java/org/mkralik/learning/axon/microservices/hotel/HotelAxonApplication.java
@@ -1,6 +1,6 @@
 package org.mkralik.learning.axon.microservices.hotel;
 
-import org.mkralik.learning.lra.axon.api.EnableLraAxonConnectorModule;
+import org.mkralik.learning.lra.axon.interceptor.EnableLraAxonConnectorModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
